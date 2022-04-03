9519c45d569268bfb3157c2a698d19a806c2a8e4
Animesh Samal <animeshsamal1995@gmail.com>
2022-04-03 12:46:29 +0530

    tikan

--- server.js
+++ server.js
@@ -74,4 +74,4 @@ io.on('connection', socket => {
 
 const PORT = process.env.PORT || 8000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT} and http://${hostname}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT} `));
